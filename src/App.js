c7962737ebc0f7b7cd36c488e82bb7ae8fd40384
Joohyun Kim <corinthionia@gmail.com>
2022-01-07 02:12:57 +0900

    :poop: [chore]: test display resolution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import EditablePage from './EditablePage';
 import GlobalStyle from './GlobalStyle';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <>
-      <GlobalStyle />
-      <EditablePage />
-    </>
+    <Wrapper>
+      {/* <GlobalStyle />
+      <EditablePage /> */}
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  width: 1920px;
+  height: 1080px;
+  background: powderblue;
+`;
 
 export default App;
