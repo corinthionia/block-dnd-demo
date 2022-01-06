8271b333defe346f24caa7a52c978a405c3064ce
Joohyun Kim <corinthionia@gmail.com>
2022-01-07 03:45:15 +0900

    :bug: ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const App = () => {
-  return <Wrapper></Wrapper>;
+  return <Wrapper>1440 * 900 크기 </Wrapper>;
 };
 
 const Wrapper = styled.div`
-  width: 1910px;
-  height: 1070px;
+  width: 1430px;
+  height: 890px;
   background: powderblue;
 
   border: 5px solid pink;
